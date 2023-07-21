9d87c90f4db1c47aa48807896bbfab0529972cbb
ptrkalm <pkalmuczak@gmail.com>
2023-07-21 19:09:22 +0200

    added new endpoint without test coverage

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ use axum::{routing::get, Router};
 
 #[shuttle_runtime::main]
 async fn axum() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello::hello_world));
+    let router = Router::new()
+        .route("/", get(hello::hello_world))
+        .route("/v2", get(hello::hello_world_2));
 
     Ok(router.into())
 }
