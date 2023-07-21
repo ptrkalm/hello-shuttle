ee5507dccbb762c5c9bbacef271ed66142dc43db
ptrkalm <pkalmuczak@gmail.com>
2023-07-21 19:05:01 +0200

    deleted code without test coverage

--- src/hello.rs
+++ src/hello.rs
@@ -1,11 +1,3 @@
 pub async fn hello_world() -> &'static str {
     "Hello, Shuttle!"
 }
-
-pub async fn hello_world_2() -> &'static str {
-    "Hello, Shuttle 2!"
-}
-
-pub async fn hello_world_3() -> &'static str {
-    "Hello, Shuttle 3!"
-}
--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,7 @@ use axum::{routing::get, Router};
 
 #[shuttle_runtime::main]
 async fn axum() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new()
-        .route("/", get(hello::hello_world))
-        .route("/v2", get(hello::hello_world_2))
-        .route("/v3", get(hello::hello_world_3));
+    let router = Router::new().route("/", get(hello::hello_world));
 
     Ok(router.into())
 }
