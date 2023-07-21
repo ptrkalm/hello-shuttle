e817af1ac249e0a0081cf55632d2e1743085602f
ptrkalm <pkalmuczak@gmail.com>
2023-07-21 02:29:33 +0200

    added third endpoint

--- src/hello.rs
+++ src/hello.rs
@@ -5,3 +5,7 @@ pub async fn hello_world() -> &'static str {
 pub async fn hello_world_2() -> &'static str {
     "Hello, Shuttle 2!"
 }
+
+pub async fn hello_world_3() -> &'static str {
+    "Hello, Shuttle 3!"
+}
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ use axum::{routing::get, Router};
 async fn axum() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
         .route("/", get(hello::hello_world))
-        .route("/v2", get(hello::hello_world_2));
+        .route("/v2", get(hello::hello_world_2))
+        .route("/v3", get(hello::hello_world_3));
 
     Ok(router.into())
 }
