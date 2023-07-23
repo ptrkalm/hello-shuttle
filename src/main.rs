471c30c2ea8ab1b335c5b1d7d485d95def99869e
ptrkalm <pkalmuczak@gmail.com>
2023-07-23 03:04:40 +0200

    created todo module

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,18 @@
 mod hello;
-use axum::{extract::Json, extract::State, routing::get, Router, response::IntoResponse};
-use mongodb::{Collection, Database};
-use serde::{Deserialize, Serialize};
+mod todo;
+use axum::{extract::Json, extract::State, response::IntoResponse, routing::get, Router};
+use mongodb::Database;
 use std::sync::Arc;
 
 #[shuttle_runtime::main]
 async fn axum(#[shuttle_shared_db::MongoDb] database: Database) -> shuttle_axum::ShuttleAxum {
-    let collection = database.collection::<Todo>("todos");
+    let collection = database.collection::<todo::Todo>("todos");
 
     let router = Router::new()
         .route("/", get(hello::hello_world))
         .route("/v2", get(hello::hello_world_2))
-        .route("/todos", get(list_todos).post(add_todo))
+        .route("/todos", get(todo::list_todos).post(todo::add_todo))
         .with_state(Arc::new(collection));
 
     Ok(router.into())
-}
-
-async fn add_todo(State(collection): State<Arc<Collection<Todo>>>, Json(todo): Json<Todo>) -> impl IntoResponse {
-    let todo_id = collection
-    .insert_one(todo, None)
-    .await
-    .unwrap();
-
-    Json(serde_json::json!(todo_id))
-}
-
-async fn list_todos(State(collection): State<Arc<Collection<Todo>>>) -> Json<Vec<Todo>> {
-    let todos = collection.find(None, None)
-    .await
-    .unwrap();
-
-    let todos = todos.deserialize_current().into_iter().collect::<Vec<Todo>>();
-
-    Json(todos)
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Todo {
-    pub note: String,
 }
